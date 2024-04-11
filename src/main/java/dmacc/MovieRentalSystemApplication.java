146721794a32eb82bfc7dab70688183f39f92a15
Riley D <111332720+ridodge@users.noreply.github.com>
2024-04-11 17:43:31 -0500

    initial commit

---
+++ src/main/java/dmacc/MovieRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalSystemApplication.class, args);
+	}
+
+}
